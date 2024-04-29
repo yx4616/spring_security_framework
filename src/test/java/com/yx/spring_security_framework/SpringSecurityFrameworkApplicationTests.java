2b13c19ae4c97b616ccbbd7452a00f1348a97952
yx4616 <yx4616@outlook.com>
2024-04-29 15:38:17 +0800

    Initial commit

---
+++ src/test/java/com/yx/spring_security_framework/SpringSecurityFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yx.spring_security_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
